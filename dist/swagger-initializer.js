e53f324a4c2a7ac854fa612dfc6c7b2df8eb1d2b
Yves Kaufmann <fxdapokalypse@googlemail.com>
2022-06-21 22:15:13 +0200

    Provide api via swagger-ui

---
+++ dist/swagger-initializer.js
@@ -0,0 +1,16 @@
+window.onload = function () {
+  //<editor-fold desc="Changeable Configuration Block">
+
+  // the following lines will be replaced by docker/configurator, when it runs in a docker-container
+  window.ui = SwaggerUIBundle({
+    url: "https://parloa.github.io/chat-api-spec/parloa-chat-v1.yml",
+    dom_id: "#swagger-ui",
+    deepLinking: true,
+    queryConfigEnabled: false,
+    presets: [SwaggerUIBundle.presets.apis, SwaggerUIStandalonePreset],
+    plugins: [SwaggerUIBundle.plugins.DownloadUrl],
+    layout: "BaseLayout",
+  });
+
+  //</editor-fold>
+};
