b08fad22a7a9b5a3b750c4d3f90485b3925eadb3
Yves Kaufmann <fxdapokalypse@googlemail.com>
2022-06-22 14:01:14 +0200

    Rename api file

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function () {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://parloa.github.io/chat-api-spec/parloa-chat-v1.yml",
+    url: "https://parloa.github.io/chat-api-spec/parloa-chat-v2.yml",
     dom_id: "#swagger-ui",
     deepLinking: true,
     queryConfigEnabled: false,
